6d7753671e91a8fdc04e4856b846c01caf97f5d5
paulino0518 <paulino0518@gmail.com>
2026-03-04 14:28:04 -0600

    Added map of entries and a get command method

--- CommandManager.java
+++ CommandManager.java
@@ -1,5 +1,23 @@
 package miniShell;
 
+import java.util.function.Consumer;
+import java.util.Map;
+import static java.util.Map.entry;
+
 public class CommandManager {
     
+    Map<String, Consumer<String>> commands = Map.ofEntries(
+        entry("hello", c -> System.out.println("Hello world")),
+        entry("ls", c -> System.out.println("ls needs to be made"))
+    );
+
+    public void getCommand(String command){
+        if(!commands.containsKey(command)){
+            System.out.println("Invalid command");
+        } else {
+            commands.get(command);
+            //.accept(command); after we have made it multi item input
+        }
+    }
+
 }
