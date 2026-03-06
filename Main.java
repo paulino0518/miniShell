674ccba93e609e3d09edb2a80e9573639baffaa0
paulino0518 <paulino0518@gmail.com>
2026-03-06 12:54:57 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    public void main(String[] args){
+        //I want to have a loop to run the program and if you type quit it will exit or whatever command Linux uses. 
+
+        //I want to iterate question for question
+        int numberOfQuestions = 2;
+
+        for(int i = 0; i < numberOfQuestions; i++){
+            //Call the question 
+
+            
+        }
+    }
+
+}
