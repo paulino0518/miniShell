198d623bdc044fab405065c67a8724ae40d5a739
paulino0518 <paulino0518@gmail.com>
2026-03-06 13:14:11 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,16 @@ public class Main{
     public void main(String[] args){
         //I want to have a loop to run the program and if you type quit it will exit or whatever command Linux uses. 
 
+        CommandManager commandManager = new CommandManager();
+
         //I want to iterate question for question
         int numberOfQuestions = 2;
 
         for(int i = 0; i < numberOfQuestions; i++){
             //Call the question 
-
+            System.our.println(commandManager.getQuestion());
+            
+            
             
         }
     }
