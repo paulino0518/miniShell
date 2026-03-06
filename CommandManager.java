4c71374878adb3142ce425384c14c807fec4d91b
paulino0518 <paulino0518@gmail.com>
2026-03-06 14:54:51 -0600

    Update CommandManager.java

--- CommandManager.java
+++ CommandManager.java
@@ -3,15 +3,18 @@ package miniShell;
 import java.util.function.Consumer;
 import java.util.Map;
 import static java.util.Map.entry;
+import java.util.Scanner;
 
 public class CommandManager {
-    
+    //Sanner scanner = new Scanner(System.in);
+
     Map<int, Consumer<String>> commands = Map.ofEntries(
         entry(0, c -> {
             String question = "Change directory to the parent of your current directory.";
             string answer = "cd ..";
 
-            //How will we get the question?
+            //How will we get the question? pass the user input ane compare it to answer. 
+            
         }),
         entry("ls", c -> System.out.println("ls needs to be made"))
     );
