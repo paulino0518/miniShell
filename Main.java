4943ace553d53f07ae7db82aa4eea098169b1ca8
paulino0518 <paulino0518@gmail.com>
2026-03-07 17:09:41 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ Whay data structure do I want to use to store th questions?  A array of Strings.
 public class Main {
     public void main(String[] args) {
         //Lets make the array of questions. 
+        String[] questionBank = 
     }
 }
