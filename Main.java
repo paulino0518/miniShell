d1b920543d8c81394deef5b0c396d700c1d0dbc8
paulino0518 <paulino0518@gmail.com>
2026-03-08 19:22:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,23 +10,22 @@ public class Main {
         };
 
         for(int i = 0; i < questionBank.length; i++){
-            String userInput = scanner.nextLine();
             boolean answerIsCorrect = false;
-            
-            if(userInput.equals("exit")){ break; }
 
             while(!answerIsCorrect){
             System.out.println(questionBank[i][0]);
                 //Print terminal area
                 System.out.print("> ");
 
-                userInput = scanner.nextLine();
+                String userInput = scanner.nextLine();
+
+                if(userInput.equals("exit")){ break; }
                 
                 if(questionBank[i][1].equals(userInput)){
                     answerIsCorrect = true;
                 } else {
                     System.out.println("Wrong");
-                    continue
+                    continue;
                 }
             }
             //Once it is correct
